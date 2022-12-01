bc444ed4789368b94ff41e7b1e0917e4d26166b8
Timothy Ravor <timothyravor@gmail.com>
2022-12-01 13:21:27 +0100

    Day 1: Complete!

---
+++ advent-timothy/1/advent1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	elfs, err := os.Open("1/inventory")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer elfs.Close()
+
+	scanner := bufio.NewScanner(elfs)
+
+	var caloriesCurrent = 0
+	var caloriesHighest = 0
+
+	for scanner.Scan() {
+
+		if len(scanner.Text()) == 0 {
+			if caloriesCurrent > caloriesHighest {
+				caloriesHighest = caloriesCurrent
+			}
+			caloriesCurrent = 0
+		}
+		conv, _ := strconv.Atoi(scanner.Text())
+		caloriesCurrent += conv
+	}
+
+	fmt.Println("CalorieElf to ask: ", caloriesHighest)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ advent-timothy/2/advent2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	elfs, err := os.Open("1/inventory")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer elfs.Close()
+
+	scanner := bufio.NewScanner(elfs)
+
+	var caloriesCurrent = 0
+	var caloriesTop = [3]int{0, 0, 0}
+
+	for scanner.Scan() {
+
+		if len(scanner.Text()) == 0 {
+			for index, entry := range caloriesTop {
+				if caloriesCurrent > entry {
+					caloriesTop[index] = caloriesCurrent
+					break
+				}
+			}
+
+			caloriesCurrent = 0
+		}
+		conv, _ := strconv.Atoi(scanner.Text())
+		caloriesCurrent += conv
+	}
+	var totalCalories = 0
+	for index, entry := range caloriesTop {
+		totalCalories += entry
+		fmt.Println("CalorieElf ", index+1, " to ask: ", entry)
+	}
+	fmt.Println("They are carrying: ", totalCalories, " calories.")
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
